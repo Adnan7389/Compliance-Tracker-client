18bbb959679d3e720d2842532fe4b88f6cba28f3
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-10-21 22:33:21 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,9 @@ export default {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
+      boxShadow: {
+        'soft': '0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)',
+      },
     },
   },
   plugins: [],
